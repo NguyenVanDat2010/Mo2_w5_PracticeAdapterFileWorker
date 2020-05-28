93351973bce0f69a503ee100716de7f21397b92c
Kira <datdhvinh@gmail.com>
2020-05-28 15:38:18 +0700

    Triển khai Adapter tính toán kích thước của file hoặc thư mục

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import calculator.Client;
+import calculator.FileCalculator;
+import calculator.FileCalculatorAdapter;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter file or folder path: ");
+        String path = scanner.nextLine();
+
+        FileCalculator fileCalculator = new FileCalculatorAdapter();
+        Client client = new Client(fileCalculator);
+        client.printFileSize(path);
+    }
+}
